1ef998d824b83808f6723e7b7fd5bceae9b5df01
Thomas Thurman <thomas@thurman.org.uk>
2003-10-14 19:27:40 +0000

    Fixed bug in the eval saving which wasn't the bug we were looking for but would have caused us trouble soon anyway.

--- gnusto-engine.js
+++ gnusto-engine.js
@@ -1,6 +1,6 @@
 // gnusto-lib.js || -*- Mode: Java; tab-width: 2; -*-
 // The Gnusto JavaScript Z-machine library.
-// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.25 2003/10/14 03:34:31 marnanel Exp $
+// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.26 2003/10/14 19:27:40 marnanel Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -19,7 +19,7 @@
 // http://www.gnu.org/copyleft/gpl.html ; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-const CVS_VERSION = '$Date: 2003/10/14 03:34:31 $';
+const CVS_VERSION = '$Date: 2003/10/14 19:27:40 $';
 const ENGINE_COMPONENT_ID = Components.ID("{bf7a4808-211f-4c6c-827a-c0e5c51e27e1}");
 const ENGINE_DESCRIPTION  = "Gnusto's interactive fiction engine";
 const ENGINE_CONTRACT_ID  = "@gnusto.org/engine;1";
@@ -1232,7 +1232,7 @@ GnustoEngine.prototype = {
 					var target = this.m_result_targets[j];
 					// FIXME: This is ugly too. Why is m_p_c back to front?
 					var args_supplied = this.m_param_counts[this.m_param_counts.length - (j+1)];
-					var eval_taken = this.m_gamestack_callbreaks[j+1];
+					var eval_taken = this.m_gamestack_callbreaks[j] - gamestack_cursor;
 
 					if (target==-1) {
 
