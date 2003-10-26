1c7eeac651f14f2fc4ca6fa18b37e1d060d6858b
Thomas Thurman <thomas@thurman.org.uk>
2003-10-26 23:24:49 +0000

    Bug 3475 partial checkin: Memory now restored. (Tested with LGOP nw/ne test.)

--- gnusto-engine.js
+++ gnusto-engine.js
@@ -1,6 +1,6 @@
 // gnusto-lib.js || -*- Mode: Java; tab-width: 2; -*-
 // The Gnusto JavaScript Z-machine library.
-// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.30 2003/10/26 22:51:08 marnanel Exp $
+// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.31 2003/10/26 23:24:49 marnanel Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -19,7 +19,7 @@
 // http://www.gnu.org/copyleft/gpl.html ; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-const CVS_VERSION = '$Date: 2003/10/26 22:51:08 $';
+const CVS_VERSION = '$Date: 2003/10/26 23:24:49 $';
 const ENGINE_COMPONENT_ID = Components.ID("{bf7a4808-211f-4c6c-827a-c0e5c51e27e1}");
 const ENGINE_DESCRIPTION  = "Gnusto's interactive fiction engine";
 const ENGINE_CONTRACT_ID  = "@gnusto.org/engine;1";
@@ -1096,6 +1096,11 @@ GnustoEngine.prototype = {
 			for (var n=0; n<16; n++) {
 					this.m_locals.push(0);
 			}
+
+			// OK. Finally, restore the memory.
+
+			this.m_memory = mem.concat(this.m_memory.slice(mem.length));
+
 	},
 
   get version() {
