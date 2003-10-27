45538a9a70559e8783bcd2a1f24c8302c7452eb5
Thomas Thurman <thomas@thurman.org.uk>
2003-10-27 00:26:19 +0000

    Bug 3475 partial checkin: Modified to require PC to be passed in.

--- gnusto-engine.js
+++ gnusto-engine.js
@@ -1,6 +1,6 @@
 // gnusto-lib.js || -*- Mode: Java; tab-width: 2; -*-
 // The Gnusto JavaScript Z-machine library.
-// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.31 2003/10/26 23:24:49 marnanel Exp $
+// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.32 2003/10/27 00:26:28 marnanel Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -19,7 +19,7 @@
 // http://www.gnu.org/copyleft/gpl.html ; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-const CVS_VERSION = '$Date: 2003/10/26 23:24:49 $';
+const CVS_VERSION = '$Date: 2003/10/27 00:26:28 $';
 const ENGINE_COMPONENT_ID = Components.ID("{bf7a4808-211f-4c6c-827a-c0e5c51e27e1}");
 const ENGINE_DESCRIPTION  = "Gnusto's interactive fiction engine";
 const ENGINE_CONTRACT_ID  = "@gnusto.org/engine;1";
@@ -1008,7 +1008,7 @@ GnustoEngine.prototype = {
 			this._initial_setup();
   },
 	
-  loadSavedGame: function ge_loadSavedGame(memLen, mem, stacksLen, stacks) {
+  loadSavedGame: function ge_loadSavedGame(memLen, mem, stacksLen, stacks, pc) {
 
 			function decodeStackInt(offset, length) {
 					var result = stacks[offset++];
@@ -1097,10 +1097,15 @@ GnustoEngine.prototype = {
 					this.m_locals.push(0);
 			}
 
-			// OK. Finally, restore the memory.
-
+			// Restore the memory.
 			this.m_memory = mem.concat(this.m_memory.slice(mem.length));
 
+			// The PC we're given is actually pointing at the varcode
+			// into which the success code must be stored. It should be 2.
+			// (This is specified by section 5.8 of the Quetzal document,
+			// version 1.4.)
+			this._varcode_set(2, this.m_memory[pc]);
+			this.m_pc = pc+1;
 	},
 
   get version() {
