9a7499aa2bee781d2dbc4ce51fedf56338395f73
Thomas Thurman <thomas@thurman.org.uk>
2003-11-18 15:51:33 +0000

    Copper trail now togglable. (This prints JIT as we pass through it.)

--- gnusto-engine.js
+++ gnusto-engine.js
@@ -1,6 +1,6 @@
 // gnusto-lib.js || -*- Mode: Java; tab-width: 2; -*-
 // The Gnusto JavaScript Z-machine library.
-// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.44 2003/11/18 15:42:08 marnanel Exp $
+// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.45 2003/11/18 15:51:45 marnanel Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -19,7 +19,7 @@
 // http://www.gnu.org/copyleft/gpl.html ; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-const CVS_VERSION = '$Date: 2003/11/18 15:42:08 $';
+const CVS_VERSION = '$Date: 2003/11/18 15:51:45 $';
 const ENGINE_COMPONENT_ID = Components.ID("{bf7a4808-211f-4c6c-827a-c0e5c51e27e1}");
 const ENGINE_DESCRIPTION  = "Gnusto's interactive fiction engine";
 const ENGINE_CONTRACT_ID  = "@gnusto.org/engine;1";
@@ -1239,10 +1239,11 @@ GnustoEngine.prototype = {
 					}
       }
 
-      // Some useful debugging code:
-			// FIXME: make this togglable (copper trail)
-      //burin('eng pc', start_pc.toString(16));
-			//burin('eng jit', jscode);
+				// Some useful debugging code:
+				if (this.m_coppertrail) {
+						burin('eng pc', start_pc.toString(16));
+						burin('eng jit', jscode);
+				}
 
       stopping = jscode();
     }
@@ -1265,6 +1266,10 @@ GnustoEngine.prototype = {
 			} else {
 					this._random_number(seed);
 			}
+	},
+
+  setCopperTrail: function ge_setCopperTrail(whether) {
+			this.m_coppertrail = whether;
 	},
 
 	////////////////////////////////////////////////////////////////
@@ -3475,6 +3480,10 @@ GnustoEngine.prototype = {
 	// having small files for saving slightly faster, and isn't
 	// really worth it. We may hardwire it on permanently.
 	m_compress_save_files: 1,
+
+	// When this is nonzero, we should print JIT information to the burin,
+	// for debugging.
+	m_coppertrail: 0,
 };
 
 ////////////////////////////////////////////////////////////////
