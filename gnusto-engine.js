a40a0dd501ddc6d147bb89727d5259bbc99f86f9
Thomas Thurman <thomas@thurman.org.uk>
2003-11-18 15:42:08 +0000

    Check for v5 instr set should have been for 5, 7 or 8.

--- gnusto-engine.js
+++ gnusto-engine.js
@@ -1,6 +1,6 @@
 // gnusto-lib.js || -*- Mode: Java; tab-width: 2; -*-
 // The Gnusto JavaScript Z-machine library.
-// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.43 2003/11/18 05:04:29 marnanel Exp $
+// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.44 2003/11/18 15:42:08 marnanel Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -19,7 +19,7 @@
 // http://www.gnu.org/copyleft/gpl.html ; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-const CVS_VERSION = '$Date: 2003/11/18 05:04:29 $';
+const CVS_VERSION = '$Date: 2003/11/18 15:42:08 $';
 const ENGINE_COMPONENT_ID = Components.ID("{bf7a4808-211f-4c6c-827a-c0e5c51e27e1}");
 const ENGINE_DESCRIPTION  = "Gnusto's interactive fiction engine";
 const ENGINE_CONTRACT_ID  = "@gnusto.org/engine;1";
@@ -1546,7 +1546,7 @@ GnustoEngine.prototype = {
 
 		// And pick up the relevant instruction set.
 
-    if (this.m_version==5) {
+    if (this.m_version==5 || this.m_version==7 || this.m_version==8) {
 				this.m_handlers = handlers_v578;
     } else if (this.m_version<9) {
 				gnusto_error(101, 'version not implemented');
