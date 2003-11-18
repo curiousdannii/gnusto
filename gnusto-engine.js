d182a701f83a3836349c83b9915b9be7256c40ba
Thomas Thurman <thomas@thurman.org.uk>
2003-11-18 05:04:29 +0000

    Oops. Removed debugging print.

--- gnusto-engine.js
+++ gnusto-engine.js
@@ -1,6 +1,6 @@
 // gnusto-lib.js || -*- Mode: Java; tab-width: 2; -*-
 // The Gnusto JavaScript Z-machine library.
-// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.42 2003/11/18 05:02:38 marnanel Exp $
+// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.43 2003/11/18 05:04:29 marnanel Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -19,7 +19,7 @@
 // http://www.gnu.org/copyleft/gpl.html ; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-const CVS_VERSION = '$Date: 2003/11/18 05:02:38 $';
+const CVS_VERSION = '$Date: 2003/11/18 05:04:29 $';
 const ENGINE_COMPONENT_ID = Components.ID("{bf7a4808-211f-4c6c-827a-c0e5c51e27e1}");
 const ENGINE_DESCRIPTION  = "Gnusto's interactive fiction engine";
 const ENGINE_CONTRACT_ID  = "@gnusto.org/engine;1";
@@ -2834,7 +2834,6 @@ GnustoEngine.prototype = {
 					} else {
 
 							if (this.m_output_to_console) {
-									dump(text);
 									this.m_console_buffer = this.m_console_buffer + text;
 							}
 
