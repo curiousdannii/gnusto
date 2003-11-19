0ad03d9e1532ce6f2f9456276b16f535e2c2f6e3
Thomas Thurman <thomas@thurman.org.uk>
2003-11-19 22:53:23 +0000

    And some debugging prints removed. Oops again.

--- gnusto-engine.js
+++ gnusto-engine.js
@@ -1,6 +1,6 @@
 // gnusto-lib.js || -*- Mode: Java; tab-width: 2; -*-
 // The Gnusto JavaScript Z-machine library.
-// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.47 2003/11/19 22:52:39 marnanel Exp $
+// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.48 2003/11/19 22:53:23 marnanel Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -19,7 +19,7 @@
 // http://www.gnu.org/copyleft/gpl.html ; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-const CVS_VERSION = '$Date: 2003/11/19 22:52:39 $';
+const CVS_VERSION = '$Date: 2003/11/19 22:53:23 $';
 const ENGINE_COMPONENT_ID = Components.ID("{bf7a4808-211f-4c6c-827a-c0e5c51e27e1}");
 const ENGINE_DESCRIPTION  = "Gnusto's interactive fiction engine";
 const ENGINE_CONTRACT_ID  = "@gnusto.org/engine;1";
@@ -3221,15 +3221,10 @@ GnustoEngine.prototype = {
 			var checksum = (this.m_original_memory[0x1c]<<8 |
 											this.m_original_memory[0x1d]);
 		
-
 			for (var i=0x40; i<this.m_original_memory.length; i++) {
 					total += this.m_original_memory[i];
 			}
 
-			dump(total.toString(16));
-			dump('\n');
-			dump(checksum.toString(16));
-			dump('\n');
 			return (total & 0xFFFF) == checksum;
 	},
 		
