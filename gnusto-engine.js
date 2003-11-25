4c5ce416bc072e93854052fb0967efa69c7bd138
Thomas Thurman <thomas@thurman.org.uk>
2003-11-25 20:11:10 +0000

    Fixed misnamed formal parameter to setCopperTrail().

--- gnusto-engine.js
+++ gnusto-engine.js
@@ -1,6 +1,6 @@
 // gnusto-lib.js || -*- Mode: Java; tab-width: 2; -*-
 // The Gnusto JavaScript Z-machine library.
-// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.53 2003/11/24 22:48:36 marnanel Exp $
+// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.54 2003/11/25 20:11:10 marnanel Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -19,7 +19,7 @@
 // http://www.gnu.org/copyleft/gpl.html ; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-const CVS_VERSION = '$Date: 2003/11/24 22:48:36 $';
+const CVS_VERSION = '$Date: 2003/11/25 20:11:10 $';
 const ENGINE_COMPONENT_ID = Components.ID("{bf7a4808-211f-4c6c-827a-c0e5c51e27e1}");
 const ENGINE_DESCRIPTION  = "Gnusto's interactive fiction engine";
 const ENGINE_CONTRACT_ID  = "@gnusto.org/engine;1";
@@ -1234,7 +1234,7 @@ GnustoEngine.prototype = {
 			}
 	},
 
-  setCopperTrail: function ge_setCopperTrail(whether) {
+  setCopperTrail: function ge_setCopperTrail(value) {
 			if (value) {
 					this.m_copperTrail = 1;
 			} else {
