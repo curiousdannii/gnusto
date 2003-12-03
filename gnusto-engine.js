4c0dfec6ba5ecbce50b4df3356c2ebd63dfdb525
Thomas Thurman <thomas@thurman.org.uk>
2003-12-03 22:32:01 +0000

    Bug 3912: m_original_memory is a copy, not a reference

--- gnusto-engine.js
+++ gnusto-engine.js
@@ -1,6 +1,6 @@
 // gnusto-lib.js || -*- Mode: Java; tab-width: 2; -*-
 // The Gnusto JavaScript Z-machine library.
-// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.65 2003/12/03 08:46:38 marnanel Exp $
+// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.66 2003/12/03 22:32:01 marnanel Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -19,7 +19,7 @@
 // http://www.gnu.org/copyleft/gpl.html ; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-const CVS_VERSION = '$Date: 2003/12/03 08:46:38 $';
+const CVS_VERSION = '$Date: 2003/12/03 22:32:01 $';
 const ENGINE_COMPONENT_ID = Components.ID("{bf7a4808-211f-4c6c-827a-c0e5c51e27e1}");
 const ENGINE_DESCRIPTION  = "Gnusto's interactive fiction engine";
 const ENGINE_CONTRACT_ID  = "@gnusto.org/engine;1";
@@ -1608,7 +1608,7 @@ GnustoEngine.prototype = {
 
 			this.m_hext_start  = this.getUnsignedWord(0x36);		
 	
- 			this.m_original_memory = this.m_memory;
+ 			this.m_original_memory = this.m_memory.slice(); // Make a copy.
 
 			// Use the correct addressing mode for this Z-machine version...
 			
