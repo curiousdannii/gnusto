6a05fe8c2221db6eb56ba0cf110e5e9980cef938
Eric Liga <naltrexone42@yahoo.com>
2005-01-25 08:15:23 +0000

    Fixed bug in Print_Table.  Needed to properly handle negative memory addresses.

--- gnusto-engine.js
+++ gnusto-engine.js
@@ -1,6 +1,6 @@
 // gnusto-lib.js || -*- Mode: Java; tab-width: 2; -*-
 // The Gnusto JavaScript Z-machine library.
-// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.108 2005/01/24 19:47:54 naltrexone42 Exp $
+// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.109 2005/01/25 08:15:23 naltrexone42 Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -18,7 +18,7 @@
 // http://www.gnu.org/copyleft/gpl.html ; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-const CVS_VERSION = '$Date: 2005/01/24 19:47:54 $';
+const CVS_VERSION = '$Date: 2005/01/25 08:15:23 $';
 const ENGINE_COMPONENT_ID = Components.ID("{bf7a4808-211f-4c6c-827a-c0e5c51e27e1}");
 const ENGINE_DESCRIPTION  = "Gnusto's interactive fiction engine";
 const ENGINE_CONTRACT_ID  = "@gnusto.org/engine;1?type=zcode";
@@ -3346,6 +3346,7 @@ GnustoEngine.prototype = {
 					var s='';
 
 					for (var x=0; x<width; x++) {
+						        if (address<0) { address &= 0xFFFF; }
 							s=s+this._zscii_char_to_ascii(this.m_memory[address++]);
 					}
 					
