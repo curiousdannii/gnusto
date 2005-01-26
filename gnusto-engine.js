2502c99f2634bbf85f40d28f2d3da1b4ac552804
Eric Liga <naltrexone42@yahoo.com>
2005-01-26 06:15:53 +0000

    Fixed masking bug in get_prop_length.

--- gnusto-engine.js
+++ gnusto-engine.js
@@ -1,6 +1,6 @@
 // gnusto-lib.js || -*- Mode: Java; tab-width: 2; -*-
 // The Gnusto JavaScript Z-machine library.
-// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.109 2005/01/25 08:15:23 naltrexone42 Exp $
+// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.110 2005/01/26 06:15:53 naltrexone42 Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -18,7 +18,7 @@
 // http://www.gnu.org/copyleft/gpl.html ; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-const CVS_VERSION = '$Date: 2005/01/25 08:15:23 $';
+const CVS_VERSION = '$Date: 2005/01/26 06:15:53 $';
 const ENGINE_COMPONENT_ID = Components.ID("{bf7a4808-211f-4c6c-827a-c0e5c51e27e1}");
 const ENGINE_DESCRIPTION  = "Gnusto's interactive fiction engine";
 const ENGINE_CONTRACT_ID  = "@gnusto.org/engine;1?type=zcode";
@@ -2922,7 +2922,7 @@ GnustoEngine.prototype = {
 
 					if (value & 0x80) {
 							// A two-byte field, so we take the bottom five bits.
-							value = value & 0x1F;
+							value = value & 0x3F;
 							
 							if (value==0) {
 									return 64;
