a1b3d6a4d73a63d30dd3631bb907bb65d34d5de8
Eric Liga <naltrexone42@yahoo.com>
2005-02-09 07:17:45 +0000

    Graceful handling of loads of savegames not matching current game. Pass 1

--- gnusto-engine.js
+++ gnusto-engine.js
@@ -1,6 +1,6 @@
 // gnusto-lib.js || -*- Mode: Java; tab-width: 2; -*-
 // The Gnusto JavaScript Z-machine library.
-// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.112 2005/02/09 05:17:23 naltrexone42 Exp $
+// $Header: /cvs/gnusto/src/xpcom/engine/gnusto-engine.js,v 1.113 2005/02/09 07:17:46 naltrexone42 Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -18,7 +18,7 @@
 // http://www.gnu.org/copyleft/gpl.html ; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-const CVS_VERSION = '$Date: 2005/02/09 05:17:23 $';
+const CVS_VERSION = '$Date: 2005/02/09 07:17:46 $';
 const ENGINE_COMPONENT_ID = Components.ID("{bf7a4808-211f-4c6c-827a-c0e5c51e27e1}");
 const ENGINE_DESCRIPTION  = "Gnusto's interactive fiction engine";
 const ENGINE_CONTRACT_ID  = "@gnusto.org/engine;1?type=zcode";
@@ -2797,6 +2797,9 @@ GnustoEngine.prototype = {
 
 			var max_chars;
 			var result;
+			if (!entered) {
+			  entered = ' ';	
+			}
 
 			if (this.m_version <= 4) {
 
